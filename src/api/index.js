b224b7914a1f6d4d2fdf0304fccdad46f4fbd5d5
Pranav <pranavgangadhara@gmail.com>
2023-09-19 18:28:35 +0530

    error resolved

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8080/api/v1';
+const url = 'http://15.207.14.201:8080/api/v1';
 const headers = (token) => ({
   headers: {
     'Content-Type': 'application/json',
